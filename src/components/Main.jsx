d8b4de5605c3166d4825436c2528815fea31ae48
ryota <ryota200309@email.com>
2025-02-24 19:08:55 +0900

    マークダウンエディタの実装

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Main.css";
+import ReactMarkdown from "react-markdown";
 
 const Main = ({ activeNote, onUpdateNote }) => {
   const onEditNote = (key, value) => {
@@ -24,15 +25,17 @@ const Main = ({ activeNote, onUpdateNote }) => {
           onChange={(e) => onEditNote("title", e.target.value)}
         />
         <textarea
-          id="content"
+          id="body"
           placeholder="内容を記入"
-          value={activeNote.content}
-          onChange={(e) => onEditNote("content", e.target.value)}
+          value={activeNote.body}
+          onChange={(e) => onEditNote("body", e.target.value)}
         ></textarea>
       </div>
       <div className="app-main-note-preview">
         <h1 className="preview-title">{activeNote.title}</h1>
-        <div className="markdown-preview">{activeNote.content}</div>
+        <div className="markdown-preview">
+          <ReactMarkdown>{activeNote.body}</ReactMarkdown>
+        </div>
       </div>
     </div>
   );
