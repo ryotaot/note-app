13b6b0ea752bfd2d0c8e7f87885b9c11d949fb80
ryota <ryota200309@email.com>
2025-02-24 13:52:20 +0900

    buttonの実装2

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <>
       <div className="App">
-        <Sidebar onAddNote={onAddNote} />
+        <Sidebar onAddNote={onAddNote} notes={notes} />
         <Main />
       </div>
     </>
